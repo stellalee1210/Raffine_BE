07863f9dc74742d05aa3fbb7caaa60fc2521a9be
Stella <yeobinlee1210@gmail.com>
2026-08-05 14:40:06 +0900

    refactor(PostResponseDTO): PostResponseDto에서 fileKey 삭제 - 필요 없음

--- src/main/java/com/example/communityapplication/dto/PostResponseDto.java
+++ src/main/java/com/example/communityapplication/dto/PostResponseDto.java
@@ -16,7 +16,6 @@ public class PostResponseDto {
     private Date date;
     private String title;
     private String content;
-    private String fileKey;
 
     public PostResponseDto(Posts posts) {
         this.id = posts.getId();
@@ -25,6 +24,5 @@ public class PostResponseDto {
         this.date = posts.getCreatedAt();
         this.title = posts.getTitle();
         this.content = posts.getContent();
-        this.fileKey = posts.getFileKey();
     }
 }
